cc9255dd30ebfa358d1eb1a0fa14c0d4c1e66bf4
João Mathias <joaomadersz@gmail.com>
2021-11-17 11:43:17 -0300

    update

--- resources/views/links-list.blade.php
+++ resources/views/links-list.blade.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Link Shortener </title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
     <style>
         body, html {
             overflow-x: hidden;
